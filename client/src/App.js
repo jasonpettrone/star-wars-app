872eda1c0ed9d792bb629c870c9c9b511b2b42e9
Jason Pettrone <jasonpettrone@gmail.com>
2022-02-06 14:05:03 -0500

    Hooked GraphQL query into the frontend.

--- client/src/App.js
+++ client/src/App.js
@@ -1,56 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_STAR_WARS_CHARACTERS = gql`
+  query {
+    star_wars_characters {
+      name
+      height
+      mass
+      homeworld {
+        name
+      }
+      birth_year
+    }
+  }
+`;
 
 function App() {
+  const { data, error, loading } = useQuery(GET_STAR_WARS_CHARACTERS);
+
+  if (error) return <h1>Error Page</h1>;
+  if (loading) return <h1>Spinner</h1>;
+
+  const { star_wars_characters } = data;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      {star_wars_characters.map((star_wars_character) => {
+        return (
+          <ul>
+            <li>--------------------------------</li>
+            <li>{star_wars_character.name}</li>
+            <li>{star_wars_character.height}</li>
+            <li>{star_wars_character.mass}</li>
+            <li>{star_wars_character.homeworld.name}</li>
+            <li>{star_wars_character.birth_year}</li>
+            <li>--------------------------------</li>
+          </ul>
+        );
+      })}
     </div>
   );
 }
