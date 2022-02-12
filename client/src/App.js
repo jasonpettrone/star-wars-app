3ae3ab3b8d2cdb8574169897de6177eacc426e6e
Jason Pettrone <jasonpettrone@gmail.com>
2022-02-12 17:42:39 -0500

    Added styling.

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { gql } from "@apollo/client";
 import useStarWarsCharacters from "./hooks/useStarWarsCharacters";
 import { Spinner } from "react-bootstrap";
 import StarWarsCharacterCardList from "./components/StarWarsCharacterCardList";
+import header from "./images/StarWarsAppHeader.png";
 
 const GET_STAR_WARS_CHARACTERS = gql`
   query Query($page: ID) {
@@ -27,37 +28,33 @@ const GET_STAR_WARS_CHARACTERS = gql`
 
 function App() {
   const [page, setPage] = useState(1);
-  const { pagination_data, star_wars_characters, error, loading } = useStarWarsCharacters(
-    GET_STAR_WARS_CHARACTERS,
-    page
-  );
+  const { pagination_data, star_wars_characters, error, loading } =
+    useStarWarsCharacters(GET_STAR_WARS_CHARACTERS, page);
 
   if (error) return <h1>Error Page</h1>;
   if (loading)
     return (
-      <div className="App">
-        <h1>Star Wars App</h1>
-        <StarWarsCharacterCardList
-          star_wars_characters={star_wars_characters}
-        />
-        <button
-          hidden={(page === 1 && star_wars_characters)}
-          className="btn btn-primary mt-3 mb-3"
-          disabled={true}
-        >
-          <Spinner animation="border" role="status">
-            <span className="visually-hidden">Loading...</span>
-          </Spinner>
-        </button>
-        <div className="center">
-          <Spinner hidden={page !== 1}
-            animation="border"
-            role="status"
+        <div className="App">
+          <img className="center-image mb-5 mt-5" src={header} alt="Star Wars App" />
+
+          <StarWarsCharacterCardList
+            star_wars_characters={star_wars_characters}
+          />
+          <button
+            hidden={page === 1 && star_wars_characters}
+            className="btn btn-warning mt-3 mb-3"
+            disabled={true}
           >
-            <span className="visually-hidden">Loading...</span>
-          </Spinner>
+            <Spinner animation="border" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </Spinner>
+          </button>
+          <div className="center-spinner">
+            <Spinner hidden={page !== 1} animation="border" role="status" variant="warning">
+              <span className="visually-hidden">Loading...</span>
+            </Spinner>
+          </div>
         </div>
-      </div>
     );
 
   const handleLoadMoreClicked = () => {
@@ -66,14 +63,12 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Star Wars App</h1>
-      <StarWarsCharacterCardList
-        star_wars_characters={star_wars_characters}
-      />
+      <img className="center-image mb-5 mt-5" src={header} alt="Star Wars App" />
+      <StarWarsCharacterCardList star_wars_characters={star_wars_characters} />
       <button
-        hidden={!(pagination_data.next)}
+        hidden={!pagination_data.next}
         onClick={handleLoadMoreClicked}
-        className="btn btn-primary mt-3 mb-3"
+        className="btn btn-warning mt-3 mb-3"
       >
         Load more
       </button>
