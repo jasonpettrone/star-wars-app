6d23c979f054eeb7370067d9c9faf0d20bb169bd
Jason Pettrone <jasonpettrone@gmail.com>
2022-02-13 12:52:51 -0500

    Refactored functionality from the App component into the List component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,77 +1,18 @@
 import React from "react";
-import { useState } from "react";
 import "./App.css";
-import { gql } from "@apollo/client";
-import useStarWarsCharacters from "./hooks/useStarWarsCharacters";
-import { Spinner } from "react-bootstrap";
 import StarWarsCharacterCardList from "./components/StarWarsCharacterCardList";
 import header from "./images/StarWarsAppHeader.png";
 
-const GET_STAR_WARS_CHARACTERS = gql`
-  query Query($page: ID) {
-    star_wars_characters(page: $page) {
-      count
-      next
-      previous
-      results {
-        name
-        homeworld {
-          name
-        }
-        height
-        mass
-        birth_year
-      }
-    }
-  }
-`;
-
 function App() {
-  const [page, setPage] = useState(1);
-  const { pagination_data, star_wars_characters, error, loading } =
-    useStarWarsCharacters(GET_STAR_WARS_CHARACTERS, page);
-
-  if (error) return <h1>Error Page</h1>;
-  if (loading)
-    return (
-        <div className="App">
-          <img className="center-image mb-5 mt-5" src={header} alt="Star Wars App" />
-
-          <StarWarsCharacterCardList
-            star_wars_characters={star_wars_characters}
-          />
-          <button
-            hidden={page === 1 && star_wars_characters}
-            className="btn btn-warning mt-3 mb-3"
-            disabled={true}
-          >
-            <Spinner animation="border" role="status">
-              <span className="visually-hidden">Loading...</span>
-            </Spinner>
-          </button>
-          <div className="center-spinner">
-            <Spinner hidden={page !== 1} animation="border" role="status" variant="warning">
-              <span className="visually-hidden">Loading...</span>
-            </Spinner>
-          </div>
-        </div>
-    );
-
-  const handleLoadMoreClicked = () => {
-    setPage((prevPage) => prevPage + 1);
-  };
 
   return (
     <div className="App">
-      <img className="center-image mb-5 mt-5" src={header} alt="Star Wars App" />
-      <StarWarsCharacterCardList star_wars_characters={star_wars_characters} />
-      <button
-        hidden={!pagination_data.next}
-        onClick={handleLoadMoreClicked}
-        className="btn btn-warning mt-3 mb-3"
-      >
-        Load more
-      </button>
+      <img
+        className="center-image mb-5 mt-5"
+        src={header}
+        alt="Star Wars App"
+      />
+      <StarWarsCharacterCardList />
     </div>
   );
 }
--- client/src/components/StarWarsCharacterCardList.js
+++ client/src/components/StarWarsCharacterCardList.js
@@ -1,12 +1,77 @@
 import { Container, Grid } from "@material-ui/core";
+import {Spinner } from "react-bootstrap"
 import StarWarsCharacterCard from "./StarWarsCharacterCard";
+import { gql } from "@apollo/client";
+import useStarWarsCharacters from "../hooks/useStarWarsCharacters";
+import { useState } from "react";
 
-function StarWarsCharacterCardList(props) {
+const GET_STAR_WARS_CHARACTERS = gql`
+  query Query($page: ID) {
+    star_wars_characters(page: $page) {
+      count
+      next
+      previous
+      results {
+        name
+        homeworld {
+          name
+        }
+        height
+        mass
+        birth_year
+      }
+    }
+  }
+`;
+
+function StarWarsCharacterCardList() {
+  const [page, setPage] = useState(1);
+  const { pagination_data, star_wars_characters, error, loading } =
+    useStarWarsCharacters(GET_STAR_WARS_CHARACTERS, page);
+
+  if (error) return <h1>Error Page</h1>;
+  if (loading)
+    return (
+      <Container maxWidth="lg">
+        <Grid container spacing={2}>
+          {star_wars_characters.map((star_wars_character) => {
+            return (
+              <Grid item key={star_wars_character.name} xs={12} md={4}>
+                <StarWarsCharacterCard character={star_wars_character} />
+              </Grid>
+            );
+          })}
+        </Grid>
+        <button
+          hidden={page === 1 && star_wars_characters}
+          className="btn btn-warning mt-3 mb-3"
+          disabled={true}
+        >
+          <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        </button>
+        <div className="center-spinner">
+          <Spinner
+            hidden={page !== 1}
+            animation="border"
+            role="status"
+            variant="warning"
+          >
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        </div>
+      </Container>
+    );
+
+  const handleLoadMoreClicked = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
 
   return (
-    <Container hidden={props.hidden} maxWidth="lg">
+    <Container maxWidth="lg">
       <Grid container spacing={2}>
-        {(props.star_wars_characters).map((star_wars_character) => {
+        {star_wars_characters.map((star_wars_character) => {
           return (
             <Grid item key={star_wars_character.name} xs={12} md={4}>
               <StarWarsCharacterCard character={star_wars_character} />
@@ -14,6 +79,13 @@ function StarWarsCharacterCardList(props) {
           );
         })}
       </Grid>
+      <button
+        hidden={!pagination_data.next}
+        onClick={handleLoadMoreClicked}
+        className="btn btn-warning mt-3 mb-3"
+      >
+        Load more
+      </button>
     </Container>
   );
 }
